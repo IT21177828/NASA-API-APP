61965f6e90222973e4c01e3da037a7c7e167c90b
naveenm99 <m.naveen@gtngroup.com>
2024-05-05 12:57:00 +0530

    cors access changed

--- backend/Server.js
+++ backend/Server.js
@@ -12,7 +12,7 @@ exports.app = app;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: ["http://localhost:3000"] }));
+app.use(cors());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
